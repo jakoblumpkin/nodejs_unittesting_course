ce3121b3a476b10b74870d3a258164c4f89dbd7e
jakob <jakob.lumpkin@ibm.com>
2021-09-03 08:57:43 -0500

    iseven

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// jshint expr: true
+
+var chai = require('chai'),
+ expect = chai.expect;
+
+chai.should();
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+describe('unit tests', function() {
+    it('should return true when number is even', function() {
+      isEven(4).should.be.true;
+    })
+
+    it('should return false when number is odd', function() {
+        expect(isEven(5)).to.be.false;
+        //Another way of doing it
+      })
+});
